f75147b8057f59f81240b996959975de46474b38
Akmeed <103463263+Akmeed@users.noreply.github.com>
2022-09-01 12:37:09 +1200

    Add files via upload

---
+++ lesson1/practice_task.py
@@ -0,0 +1,53 @@
+# Ahmad Algebawi
+# variables, output, labels, data types
+
+# declare variabls
+name = "Ahmad Algebawi"
+height = 1.7 
+address = "170 couts street"
+telephonenumber = "0224142924"
+allergy = "no allergy"
+
+# a summary of this information should be printed out on the screen 
+print("Name: ", name)
+print("Height: ", height)
+print("Address: ", address)
+print("Telephonenumber ", telephonenumber)
+print("Allergy ", allergy)
+
+# Clarifying the problem 
+# Hight in meters
+# Address to be stored as street number and street 
+# Countrey not required 
+# No detail of allergy type
+# Data yo be not displayed in context of readble language
+
+# Design a solition / algorithim
+# The veriblse declared above are not to be sotred as data
+# These variables are draft and should be decided on before finalised
+# Data type of hight to be stored as "float" 
+# Mainly becuase of the discreet decimal of this measurment
+
+
+
+
+
+
+
+
+# Final variables 
+
+
+
+
+
+# The readable data framed in labels 
+
+
+
+
+
+
+# Documentaion
+# At this point it should be done above
+# Critical analy
